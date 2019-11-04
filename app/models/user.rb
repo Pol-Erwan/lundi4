537ad7cbb9d7f4a0a0299d5f00244516852e39fd
Pol-Erwan <polerwankite@gmail.com>
2019-11-04 15:30:19 +0100

    validations 15h29

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+has_many :events, trouhgt: :attendance
+has_many :attendances
+
+  validates :email,
+  validates :fisrtname, length: { minimum: 2 }
+  validates :lastname, length: { minimum: 2 }
+  validates :password, length: { in: 6..20 }
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
 end
